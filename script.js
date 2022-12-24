90b8eeefca1f60f5147cb55fcf5c2f1e2642c8c1
4reha <ael-hadd@student.1337.ma>
2022-12-24 17:22:52 +0100

    Adding more function and importing questions

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+import questions from "./questions.json" assert { type: "json" };
 const statement = document.querySelector("#statement");
 const optionButtons = document.querySelector("#options").children;
 const explanation = document.querySelector("#explanation");
+const nextBtn = document.querySelector("#next");
+let fact = {};
 
-const fact = {
-    statement: "The Earth is round",
-    answer: true,
-    explanation: "The Earth is round because it is a sphere."
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
 }
 
-statement.textContent = fact.statement;
+function getReset() {
+    disable(nextBtn);
+    enable(optionButtons[0]);
+    enable(optionButtons[1]);
+    for (let button of optionButtons) {
+        button.classList.remove("correct");
+        button.classList.remove("incorrect");
+    }
+    explanation.textContent = "";
+}
+
+function getQuestion() {
+    getReset();
+    if (questions.length)   {
+        let index = getRandomInt(questions.length);
+        fact = questions.splice(index, 1)[0];
+        statement.textContent = fact.statement;
+    }
+    else {
+        statement.textContent = "No more questions :P";
+        disable(optionButtons[0]);
+        disable(optionButtons[1]);
+        disable(nextBtn);
+        nextBtn.textContent = "No More";
+        nextBtn.classList.add("nextDisabled");
+    }
+}
 
 function disable(button) {
     button.setAttribute("disabled", "");
@@ -23,8 +50,10 @@ function isCorrect(guess) {
 for (let button of optionButtons) {
     button.addEventListener("click", function(event) {
         explanation.textContent = fact.explanation;
-        for (let btn of optionButtons)
+        for (let btn of optionButtons)  {
             disable(btn);
+            enable(nextBtn);
+        }
         let guess = button.value;
         if (isCorrect(guess))
             event.target.classList.add("correct");
@@ -32,7 +61,10 @@ for (let button of optionButtons) {
             event.target.classList.add("incorrect");
     })
 }
+nextBtn.addEventListener("click", function(event) {
+    getQuestion();
+});
 
-
+getQuestion();
 
 
